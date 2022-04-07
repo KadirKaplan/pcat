26a9cbc55b46fd59ee2b5a7f9967b2c696a17684
KadirKaplan <kadir.kaplan13@gmail.com>
2022-04-07 23:17:40 +0300

    static files

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+//middlewares
+app.use(express.static('public'));
+
+
+
+app.get('/', (req, res) => {
+   res.sendFile(path.resolve(__dirname,'temp/index.html'));
+});
+
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} 'de çalışıyor...`);
+});
