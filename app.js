e25b75b180088448b1ce33927323de479ba66ca9
KadirKaplan <kadir.kaplan13@gmail.com>
2022-04-09 11:39:12 +0300

    ready for heroku

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
-const fs = require('fs');   
+const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 const Photo = require('./models/Photo');
+const methodOverride = require('method-override');
+const photoController = require('./controller/photoControllers');
+const pageController = require('./controller/pageController');
+
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/PCAT');
+mongoose.connect('mongodb+srv://kadirkaplan:qweasd123@cluster0.yhbwx.mongodb.net/PCAT?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then (() => {
+    console.log("db connected!")
+}).catch ((err) => {
+    console.log(err)
+});
+
 
 //TEMPLATEENGİNE(ejs)
 app.set("view engine", "ejs");
@@ -18,57 +31,26 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));// urldeki datayı okumamızı saglıyor
 app.use(express.json());// buda urldeki datayı json formatına donusturmeye yarıyor.
 app.use(fileUpload());
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}
+));
+
 //ROUTES
 
-app.get('/', async (req, res) => {
-    const photos = await Photo.find({}).sort('-dateCreated');
-    res.render('index', {
-        photos
-    });
-});
-app.get('/photos/:id', async (req, res) => {
-
-    const photo = await Photo.findById(req.params.id);
-    res.render('photo', {
-        photo
-    });
-});
-app.get('/about', (req, res) => {
-
-    res.render('about');
-});
-
-app.get('/add', (req, res) => {
-
-    res.render('add');
-});
-
-app.post('/photos', async (req, res) => {
-    
-
-  
-    const uploadDir = 'public/uploads';
-  
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir);
-    }
-  
-    let uploadeImage = req.files.image;
-    let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
-  
-    uploadeImage.mv(uploadPath, async () => {
-      await Photo.create({
-        ...req.body,
-        image: '/uploads/' + uploadeImage.name,
-      });
-      res.redirect('/');
-    });
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 
 
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Sunucu ${port} 'de çalışıyor...`);
 });
